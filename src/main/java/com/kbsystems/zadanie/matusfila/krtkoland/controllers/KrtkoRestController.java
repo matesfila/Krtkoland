6951ab6d3cfc5d5f0f2ff3b6df7234d833a01193
matesfila <matesfila@gmail.com>
2025-05-26 12:26:17 +0200

    presmerovanie rest url na "api" doplnenie @RequestParam

--- src/main/java/com/kbsystems/zadanie/matusfila/krtkoland/controllers/KrtkoRestController.java
+++ src/main/java/com/kbsystems/zadanie/matusfila/krtkoland/controllers/KrtkoRestController.java
@@ -4,24 +4,27 @@ import com.kbsystems.zadanie.matusfila.krtkoland.models.BestTimeAndPathResult;
 import com.kbsystems.zadanie.matusfila.krtkoland.services.KrtkoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * matus 5/17/2025
  */
 @RestController
+@RequestMapping("/api")
 public class KrtkoRestController {
 
     @Autowired
     KrtkoService krtkoService;
 
-    @GetMapping("/getTest")
-    public String getTest(String s) {
+    @GetMapping("/healthCheck")
+    public String healthCheck(String s) {
         return "toto je testovacia metoda " + s;
     }
 
     @GetMapping("/getBestPath")
-    public BestTimeAndPathResult getBestPath(String source, String target) {
+    public BestTimeAndPathResult getBestPath(@RequestParam String source, @RequestParam String target) {
         return krtkoService.theBestTimeAndPath(source, target);
     }
 
